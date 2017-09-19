fbc00ccf9582586d9bc0d0bf2f967446cc459848
Andy Chen <amazingandyyy@gmail.com>
2017-09-19 09:30:15 -0700

    reset to the notes

--- map/main.go
+++ map/main.go
@@ -6,22 +6,19 @@ func main() {
 	// 1: var colors map[string]string
 	// 2: colors := make(map[string]string)
 	// 3:
-
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"black": "#000000",
+		"green": "$4b745",
+	}
 	// > colors["white"] = "#ffffff" // use [] to access the value
 	// > delete(colors, "white")
-	myColorsList.printMap()
+
+	printMap(colors)
 }
 
-type colors map[string]string
-
-func (c colors) printMap() {
+func printMap(c map[string]string) {
 	for color, hex := range c {
 		fmt.Println("Hex code for", color, "is", hex)
 	}
-}
-
-var myColorsList = colors{
-	"red":   "#ff0000",
-	"black": "#000000",
-	"green": "$4b745",
 }
